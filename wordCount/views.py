0834cdada251a5ec71dc57f5b0af85faa7b9559d
hitesh <hiteshgohil@hotmail.co.uk>
2019-05-24 21:26:36 +0100

    basic word count added

--- wordCount/views.py
+++ wordCount/views.py
@@ -9,4 +9,6 @@ def about(request):
     return HttpResponse('<em>Aboutttt</em>')
 
 def count(request):
-    return render(request, 'count.html')
+    fulltext = request.GET['fulltext']
+    wordlist = fulltext.split()
+    return render(request, 'count.html', {'fulltext': fulltext, 'count': len(wordlist)})
